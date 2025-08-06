3cbdd0815d794d46e67b53a8f03b043dc09b49b4
Ayub40 <ayubk4028@gmail.com>
2025-08-06 22:39:15 +0600

    dark mode added

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,5 +1,5 @@
 // import Logo from "@/registry/default/components/navbar-components/logo"
-import Logo from "@/assets/icons/Logo" 
+import Logo from "@/assets/icons/Logo"
 import { Button } from "@/components/ui/button"
 import {
   NavigationMenu,
@@ -12,13 +12,15 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import { ModeToggle } from "./ModeToggler"
+import { Link } from "react-router"
 
 // Navigation links array to be used in both desktop and mobile menus
 const navigationLinks = [
-  { href: "#", label: "Home", active: true },
-  { href: "#", label: "Features" },
-  { href: "#", label: "Pricing" },
-  { href: "#", label: "About" },
+  { href: "/", label: "Home", active: true },
+  { href: "/about", label: "About" },
+  // { href: "#", label: "Pricing" },
+  // { href: "#", label: "About" },
 ]
 
 export default function Navbar() {
@@ -62,6 +64,7 @@ export default function Navbar() {
                 </svg>
               </Button>
             </PopoverTrigger>
+            {/* For mobile */}
             <PopoverContent align="start" className="w-36 p-1 md:hidden">
               <NavigationMenu className="max-w-none *:w-full">
                 <NavigationMenuList className="flex-col items-start gap-0 md:gap-2">
@@ -90,12 +93,10 @@ export default function Navbar() {
               <NavigationMenuList className="gap-2">
                 {navigationLinks.map((link, index) => (
                   <NavigationMenuItem key={index}>
-                    <NavigationMenuLink
-                      active={link.active}
-                      href={link.href}
+                    <NavigationMenuLink asChild
                       className="text-muted-foreground hover:text-primary py-1.5 font-medium"
                     >
-                      {link.label}
+                      <Link to={link.href}> {link.label}</Link>
                     </NavigationMenuLink>
                   </NavigationMenuItem>
                 ))}
@@ -105,11 +106,9 @@ export default function Navbar() {
         </div>
         {/* Right side */}
         <div className="flex items-center gap-2">
-          <Button asChild variant="ghost" size="sm" className="text-sm">
-            <a href="#">Sign In</a>
-          </Button>
-          <Button asChild size="sm" className="text-sm">
-            <a href="#">Get Started</a>
+          <ModeToggle />
+          <Button asChild className="text-sm">
+            <a href="/login">Login</a>
           </Button>
         </div>
       </div>
