dd83fe500ccc4d51923f672119c65868621ab079
Ayub40 <ayubk4028@gmail.com>
2025-08-10 23:32:57 +0600

    admin dashboard created

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import App from "@/App";
 import DashboardLayout from "@/components/layout/DashboardLayout";
 import About from "@/pages/About";
-import Analytics from "@/pages/Admin/Analytics";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
 import Bookings from "@/pages/User/Bookings";
 import Verify from "@/pages/Verify";
+import { generateRoutes } from "@/utils/generateRoutes";
 import { createBrowserRouter } from "react-router";
+import { adminSidebarItems } from "./adminSidebarItems";
 
 export const router = createBrowserRouter([
   {
@@ -23,13 +24,9 @@ export const router = createBrowserRouter([
   {
     Component: DashboardLayout,
     path: "/admin",
-    children: [
-      {
-        Component: Analytics,
-        path: "analytics"
-      }
-    ]
+    children: [...generateRoutes(adminSidebarItems)]
   },
+  // ====================================
   {
     Component: DashboardLayout,
     path: "/user",
